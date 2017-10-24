5576b8409976f2aa42510528d66998b75d6083ca
yinzuolong <yinzuolong@ppdai.com>
2017-10-24 16:32:30 +0800

    增加ResourceTest

---
+++ util_test/src/test/java/com/yzl/spring/ResourceTest.java
@@ -0,0 +1,22 @@
+package com.yzl.spring;
+
+import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.StreamUtils;
+
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Created by yinzuolong on 2017/10/24.
+ */
+public class ResourceTest {
+    @Test
+    public void testClassPathResource() throws Exception {
+        ClassPathResource resource = new ClassPathResource("/readme.txt");
+        try (InputStream in = resource.getInputStream()) {
+            String txt = StreamUtils.copyToString(in, StandardCharsets.UTF_8);
+            System.out.println(txt);
+        }
+    }
+}
