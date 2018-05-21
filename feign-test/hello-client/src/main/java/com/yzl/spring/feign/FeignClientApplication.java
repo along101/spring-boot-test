ab06698dc47d76a27401c3cb265a2b1cc9f56f33
yinzuolong <yinzuolong@ppdai.com>
2018-05-21 13:42:18 +0800

    feign增加header设置

--- feign-test/hello-client/src/main/java/com/yzl/spring/feign/FeignClientApplication.java
+++ feign-test/hello-client/src/main/java/com/yzl/spring/feign/FeignClientApplication.java
@@ -1,16 +1,33 @@
 package com.yzl.spring.feign;
 
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
+@Configuration
 public class FeignClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FeignClientApplication.class, args);
     }
+
+    @Bean
+    public RequestInterceptor createMyRequestInterceptor() {
+        RequestInterceptor requestInterceptor = new RequestInterceptor() {
+            @Override
+            public void apply(RequestTemplate template) {
+                template.header("x-my-header", "test");
+            }
+        };
+        return requestInterceptor;
+    }
+
 }
--- feign-test/hello-service/src/main/java/com/yzl/spring/feign/config/MyConfig.java
+++ feign-test/hello-service/src/main/java/com/yzl/spring/feign/config/MyConfig.java
@@ -2,11 +2,40 @@ package com.yzl.spring.feign.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author yinzuolong
  */
 @Configuration
 @Import(HandlerMappingPostProcessor.class)
-public class MyConfig {
+public class MyConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new HandlerInterceptor() {
+
+            @Override
+            public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+                System.out.println(request.getHeader("x-my-header"));
+                return true;
+            }
+
+            @Override
+            public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+            }
+
+            @Override
+            public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+            }
+        }).addPathPatterns("/**");
+    }
+
 }
