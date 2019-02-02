665912fe2df684a0b95da73e596e924ec6e0e7a6
zhangchengxi <orezsilence@163.com>
2019-02-02 18:53:19 +0800

    使用 ClassPathResource 简化代码

--- spring-boot-properties/src/test/java/com/yzl/test/PropertiesBindTest.java
+++ spring-boot-properties/src/test/java/com/yzl/test/PropertiesBindTest.java
@@ -4,11 +4,10 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.boot.bind.PropertySourcesBinder;
 import org.springframework.core.env.MapPropertySource;
-import org.springframework.core.io.UrlResource;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.support.PropertiesLoaderUtils;
 
 import java.io.IOException;
-import java.net.URL;
 import java.util.Map;
 import java.util.Properties;
 import java.util.function.Function;
@@ -21,23 +20,20 @@ public class PropertiesBindTest {
 
     @Test
     public void testBind() throws IOException {
-        URL resources = Thread.currentThread().getContextClassLoader().getResource("my.properties");
-        Properties properties = PropertiesLoaderUtils.loadProperties(new UrlResource(resources));
-        Map<String, String> map = properties.stringPropertyNames().stream().collect(Collectors.toMap(Function.identity(), properties::getProperty));
+        Properties properties = PropertiesLoaderUtils.loadProperties(new ClassPathResource("my.properties"));
+        Map<String, Object> map = properties.stringPropertyNames().stream().collect(Collectors.toMap(Function.identity(), properties::getProperty));
 
-        MapPropertySource mapPropertySource = new MapPropertySource("myMap", (Map) map);
+        MapPropertySource mapPropertySource = new MapPropertySource("myMap", map);
         PropertySourcesBinder propertySourcesBinder = new PropertySourcesBinder(mapPropertySource);
         MyProperties config = new MyProperties();
         propertySourcesBinder.bindTo("yzl", config);
 
         Assert.assertEquals("yzl123", config.getStr());
         Assert.assertEquals("test3", config.getStrList().get(2));
-        Assert.assertTrue(123 == config.getSubProperties().get(0).getSubInteger());
-        Assert.assertEquals("test11" , config.getSubMapStr().get("key1.key1.key1"));
-        Assert.assertEquals("test22" , config.getSubMap().get("key1").getSubStr());
-        Assert.assertEquals("test13123" , config.getSubMap().get("com.yzl.test1.Test").getSubStr());
-
-
+        Assert.assertEquals(123, (int) config.getSubProperties().get(0).getSubInteger());
+        Assert.assertEquals("test11", config.getSubMapStr().get("key1.key1.key1"));
+        Assert.assertEquals("test22", config.getSubMap().get("key1").getSubStr());
+        Assert.assertEquals("test13123", config.getSubMap().get("com.yzl.test1.Test").getSubStr());
 
     }
 }
