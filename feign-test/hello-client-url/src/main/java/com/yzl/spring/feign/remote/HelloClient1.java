8f3a7a0b9d188facefeb675ec7e89fb473f1be30
yutu <zuolong.yzl@alibaba-inc.com>
2021-03-22 11:44:56 +0800

    修改测试

--- feign-test/hello-client-url/src/main/java/com/yzl/spring/feign/remote/HelloClient1.java
+++ feign-test/hello-client-url/src/main/java/com/yzl/spring/feign/remote/HelloClient1.java
@@ -3,7 +3,7 @@ package com.yzl.spring.feign.remote;
 import com.yzl.spring.feign.service.HelloService1;
 import org.springframework.cloud.netflix.feign.FeignClient;
 
-@FeignClient(url = "${remote.hello.service.host}")
+@FeignClient(name = "test1", url = "${remote.hello.service.host}")
 public interface HelloClient1 extends HelloService1 {
 
 
--- feign-test/hello-client-url/src/main/java/com/yzl/spring/feign/remote/HelloClient2.java
+++ feign-test/hello-client-url/src/main/java/com/yzl/spring/feign/remote/HelloClient2.java
@@ -3,7 +3,7 @@ package com.yzl.spring.feign.remote;
 import com.yzl.spring.feign.service.HelloService2;
 import org.springframework.cloud.netflix.feign.FeignClient;
 
-@FeignClient(url = "${remote.hello.service.host}")
+@FeignClient(name = "test2", url = "${remote.hello.service.host}")
 public interface HelloClient2 extends HelloService2 {
 
 
